525bc8fb1ece9097bb5b60573761c747c72f936f
Malyshko Serhii <malyshkosergiy@gmail.com>
2021-01-03 00:56:32 +0200

    click on image has been added as an exapmle (for MoviesGallery component)

--- src/components/MovieGallery/MovieGallery.js
+++ src/components/MovieGallery/MovieGallery.js
@@ -4,6 +4,14 @@ import s from './MovieGallery.module.css';
 const MovieCard = ({ movie }) => {
   const { url } = useRouteMatch();
 
+  // return (
+  //   <Link to={`${url}/${movie.id}`} className={s.MovieCard}>
+  //     <img
+  //       src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+  //       alt={movie.title ? movie.title : movie.name}
+  //     />
+  //   </Link>
+  // );
   return url === '/' ? (
     <Link to={`${movie.id}`} className={s.MovieCard}>
       {movie.title ? movie.title : movie.name}
