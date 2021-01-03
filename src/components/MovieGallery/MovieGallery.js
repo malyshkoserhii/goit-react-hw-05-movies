72b618c1e7f5659d6bbf956a3a24726bab8d4eb0
Malyshko Serhii <malyshkosergiy@gmail.com>
2021-01-03 16:06:53 +0200

    base styling has been finished

--- src/components/MovieGallery/MovieGallery.js
+++ src/components/MovieGallery/MovieGallery.js
@@ -3,41 +3,46 @@ import s from './MovieGallery.module.css';
 
 const MovieCard = ({ movie }) => {
   const { url } = useRouteMatch();
+  const backdropImage = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+  const defaultImage =
+    'https://i.dlpng.com/static/png/1330322-minion-concerned-minion-png-400_400_preview.png';
+  const backdrop = movie.backdrop_path ? backdropImage : defaultImage;
 
   return url === '/' ? (
     <Link to={`${movie.id}`} className={s.MovieCard}>
       <>
         <div className={s.imageWrapper}>
           <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            src={backdrop}
             alt={movie.title ? movie.title : movie.name}
+            className={
+              movie.backdrop_path ? s.galleryImage : s.defaultGalleryImage
+            }
           />
         </div>
-        <p>{movie.title ? movie.title : movie.name}</p>
+        <p className={s.galleryMovieTitle}>
+          {movie.title ? movie.title : movie.name}
+        </p>
       </>
     </Link>
   ) : (
     <Link to={`${url}/${movie.id}`} className={s.MovieCard}>
       <>
-        <div>
+        <div className={s.imageWrapper}>
           <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            src={backdrop}
             alt={movie.title ? movie.title : movie.name}
+            className={
+              movie.backdrop_path ? s.galleryImage : s.defaultGalleryImage
+            }
           />
         </div>
-        <p>{movie.title ? movie.title : movie.name}</p>
+        <p className={s.galleryMovieTitle}>
+          {movie.title ? movie.title : movie.name}
+        </p>
       </>
     </Link>
   );
-  // return url === '/' ? (
-  //   <Link to={`${movie.id}`} className={s.MovieCard}>
-  //     {movie.title ? movie.title : movie.name}
-  //   </Link>
-  // ) : (
-  //   <Link to={`${url}/${movie.id}`} className={s.MovieCard}>
-  //     {movie.title ? movie.title : movie.name}
-  //   </Link>
-  // );
 };
 
 export default MovieCard;
